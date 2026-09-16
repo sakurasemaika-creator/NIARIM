66533e2d49da4ef848815de5897f6d51fbbf77a6
sakurasemaika-creator <sakurasemaika@gmail.com>
2026-09-16 19:19:59 +0900

    test: require pixelate filter to use shared pixel art engine

---
+++ test/engine/filter_pixel_art_route_test.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('pixelate filter routes through the shared PixelArtEngine', () {
+    final source = File('lib/engine/filter_engine.dart').readAsStringSync();
+
+    expect(source, contains("import 'pixel_art_engine.dart';"));
+    expect(
+      source,
+      contains('const PixelArtEngine().convert('),
+    );
+  });
+}
