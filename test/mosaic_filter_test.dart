48dc3be6f9206e70ddf7c87798589b09443d52cd
sakurasemaika-creator <sakurasemaika@gmail.com>
2026-09-17 01:47:36 +0900

    test: align mosaic regression with compatibility API

--- test/mosaic_filter_test.dart
+++ test/mosaic_filter_test.dart
@@ -13,17 +13,11 @@ void main() {
       100, 100, 0, 255,
     ]);
 
-    final result = FilterEngine().applyMosaic(
-      input,
-      2,
-      2,
-      blockSize: 2,
-    );
+    final result = FilterEngine().applyMosaic(input, 2, 2, 2);
 
-    expect(
-      result,
-      Uint8List.fromList(List<int>.generate(4, (_) => 0).expand((_) => [50, 50, 0, 255]).toList()),
-    );
+    for (var i = 0; i < result.length; i += 4) {
+      expect(result.sublist(i, i + 4), [50, 50, 0, 255]);
+    }
   });
 
   test('draw-filter isolate dispatches mosaic to classic mosaic path', () {
