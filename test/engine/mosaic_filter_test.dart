60ccbece63839974b5cad0bc9fc4c4f1a549010c
sakurasemaika-creator <sakurasemaika@gmail.com>
2026-09-17 10:19:35 +0900

    test: lock classic mosaic behavior on dev_branch

---
+++ test/engine/mosaic_filter_test.dart
@@ -0,0 +1,19 @@
+import 'dart:typed_data';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:niarim/engine/filter_engine.dart';
+
+void main() {
+  test('classic mosaic averages RGBA independently from pixel-art conversion', () {
+    final input = Uint8List.fromList([
+      255, 0, 0, 255,
+      0, 255, 0, 255,
+      0, 0, 255, 0,
+      255, 255, 255, 0,
+    ]);
+
+    final output = FilterEngine().applyMosaic(input, 2, 2, 2);
+
+    expect(output, [128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128]);
+  });
+}
