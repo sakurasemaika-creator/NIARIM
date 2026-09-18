fadc3894144200a20b3ffef48e85abf489bc9326
sakurasemaika-creator <sakurasemaika@gmail.com>
2026-09-18 16:29:08 +0900

    test: premultiply translucent blend fixture pixels

--- test/blend_mode_math_fixture_test.dart
+++ test/blend_mode_math_fixture_test.dart
@@ -54,8 +54,8 @@ Future<List<int>> _compositePixel(
   List<int> source,
 ) async {
   final tm = TileManager(canvasWidth: 1, canvasHeight: 1);
-  tm.replaceLayerPixels('scene#0#backdrop', Uint8List.fromList(backdrop));
-  tm.replaceLayerPixels('scene#0#source', Uint8List.fromList(source));
+  tm.replaceLayerPixels('scene#0#backdrop', _premultiply(backdrop));
+  tm.replaceLayerPixels('scene#0#source', _premultiply(source));
   final image = await LayerCompositor.composite(
     tm,
     <Layer>[
@@ -79,6 +79,16 @@ Future<List<int>> _compositePixel(
   image.dispose();
   tm.dispose();
   return data!.buffer.asUint8List().take(4).toList();
+}
+
+Uint8List _premultiply(List<int> straight) {
+  final alpha = straight[3] / 255.0;
+  return Uint8List.fromList(<int>[
+    (straight[0] * alpha).round(),
+    (straight[1] * alpha).round(),
+    (straight[2] * alpha).round(),
+    straight[3],
+  ]);
 }
 
 List<int> _reference(
